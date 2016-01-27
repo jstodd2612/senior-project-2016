b9027b730eab7959ac4c03e9b97b1b571d69e285
jstodd <jstodd2612@gmail.com>
2016-01-26 22:04:51 -0700

    Added Google/FBook login buttons.

--- www/js/app.js
+++ www/js/app.js
@@ -115,6 +115,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/homework');
+  $urlRouterProvider.otherwise('tab/homework');
 
 });
