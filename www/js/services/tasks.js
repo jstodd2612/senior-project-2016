bec3f74dc2d2501fc13c1141eb166321788e0b04
ksmithut <ksmith.ut@gmail.com>
2016-04-11 23:26:59 -0600

    updates tasks to reflect new structure

--- www/js/services/tasks.js
+++ www/js/services/tasks.js
@@ -6,42 +6,53 @@ angular.module('tasks', [
 .factory('tasks', [
   '$firebaseObject',
   '$firebaseArray',
+  '$q',
   'firebaseInstance',
   'users',
-  function($firebaseObject, $firebaseArray, firebase, users) {
-
-    var tasks = firebase.child('tasks')
+  function($firebaseObject, $firebaseArray, $q, firebase, users) {
 
     function createTask(data) {
       return users.getCurrentUser()
         .then(function(user) {
-          return $firebaseArray(tasks).$add({
-            title: data.title || null,
-            content: data.content || null,
-            createdBy: user.id,
-            assigned: user.id || null,
-            type: data.type,
-            category: data.category || null,
-            dueDate: data.dueDate || null,
-            completed: false,
-            archived: false,
-            quantity: data.quantity || null
-          })
+          let userTasks = firebase.child('users/' + user.id + '/tasks')
+          let $userTasks = $firebaseArray(userTasks)
+          return $userTasks
+            .$add({
+              title: data.title || null,
+              createdBy: user.id,
+              type: data.type || null,
+              category: data.category || null,
+              dueDate: data.dueDate || null,
+              archived: false,
+              subTasks: null
+            })
+            .then(function(task) {
+              let subTasks = userTasks.child(task.key() + '/subTasks')
+              let $subTasks = $firebaseArray(subTasks)
+              let inputSubTasks = data.subTasks || []
+              return $q
+                .all(inputSubTasks.map(function(subTask, i) {
+                  subTask.order = 'order' in subTask ? subTask.order : i
+                  subTask.completed = subTask.completed || false
+                  return $subTasks.$add(subTask)
+                }))
+                .then(function () {
+                  return $userTasks.$getRecord(task.key())
+                })
+            })
         })
         .then(function(newTask) {
-          return newTask
+          return newTask.$value
         })
     }
 
-    function getTasksByUser(userId, type) {
-      return $firebaseArray(tasks).$loaded()
-        .then(function(arr) {
-          // This is the array of tasks, but it returns all of them...
-          // You'll have to filter them manually for now.
-          return arr.filter(function(task) {
-            return task.createdBy === userId && task.type === type && task.archived === false
-          })
-        })
+    function updateTask() {
+
+    }
+
+    function getTasksByUser(userId) {
+      let userTasks = firebase.child('users/' + userId + '/tasks')
+      return $firebaseArray(userTasks).$loaded()
     }
 
     // function archiveTask(userId, taskId) {
