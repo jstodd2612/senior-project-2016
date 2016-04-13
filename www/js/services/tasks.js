e2ef2563ef72dd8b8a68b9a4b8d66379a2d66cf0
J Stodd <jstodd2612@gmail.com>
2016-04-12 21:49:21 -0600

    Other minor changes, added archive button on todos

--- www/js/services/tasks.js
+++ www/js/services/tasks.js
@@ -14,22 +14,22 @@ angular.module('tasks', [
     function createTask(data) {
       return users.getCurrentUser()
         .then(function(user) {
-          let userTasks = firebase.child('users/' + user.id + '/tasks')
-          let $userTasks = $firebaseArray(userTasks)
+          var userTasks = firebase.child('users/' + user.id + '/tasks')
+          var $userTasks = $firebaseArray(userTasks)
           return $userTasks
             .$add({
               title: data.title || null,
               createdBy: user.id,
-              type: data.type || null,
+              type: data.type,
               category: data.category || null,
               dueDate: data.dueDate || null,
               archived: false,
-              subTasks: null
+              subTasks: data.subTasks || null
             })
             .then(function(task) {
-              let subTasks = userTasks.child(task.key() + '/subTasks')
-              let $subTasks = $firebaseArray(subTasks)
-              let inputSubTasks = data.subTasks || []
+              var subTasks = userTasks.child(task.key() + '/subTasks')
+              var $subTasks = $firebaseArray(subTasks)
+              var inputSubTasks = data.subTasks || []
               return $q
                 .all(inputSubTasks.map(function(subTask, i) {
                   subTask.order = 'order' in subTask ? subTask.order : i
@@ -50,9 +50,16 @@ angular.module('tasks', [
       // TODO
     }
 
-    function getTasksByUser(userId) {
-      let userTasks = firebase.child('users/' + userId + '/tasks')
+    function getTasksByUser(userId, type) {
+      var userTasks = firebase.child('users/' + userId + '/tasks')
       return $firebaseArray(userTasks).$loaded()
+        .then(function(arr) {
+          // This is the array of tasks, but it returns all of them...
+          // You'll have to filter them manually for now.
+          return arr.filter(function(userTask) {
+            return userTask.createdBy === userId && userTask.type === type && userTask.archived === false
+          })
+        })
     }
 
     // function archiveTask(userId, taskId) {
