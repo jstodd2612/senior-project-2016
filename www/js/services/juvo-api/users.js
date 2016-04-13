dd5f950e995a6da195dd8163ce93a2bf2d21c198
ksmithut <ksmith.ut@gmail.com>
2016-04-13 01:22:04 -0600

    adds juvo api services

---
+++ www/js/services/juvo-api/users.js
@@ -0,0 +1,57 @@
+angular
+.module('juvo-api.users', [
+  'juvo-api'
+])
+.service('juvoUsers', [
+  '$http',
+  'juvoApiHost',
+  function($http, host) {
+
+    function returnData(response) {
+      return response.data
+    }
+
+    this.create = function(data) {
+      return $http
+        .post(host + '/users', data)
+        .then(returnData)
+    }
+
+    this.list = function(data) {
+      return $http
+        .get(host + '/users', data)
+        .then(returnData)
+    }
+
+    this.update = function(id, data) {
+      return $http
+        .put(host + '/users/' + id, data)
+        .then(returnData)
+    }
+
+    this.remove = function(id) {
+      return $http
+        .delete(host + '/users/' + id)
+        .then(returnData)
+    }
+
+    this.invite = function(email) {
+      return $http
+        .post(host + '/invites', { email: email })
+        .then(returnData)
+    }
+
+    this.listInvites = function() {
+      return $http
+        .get(host + '/invites')
+        .then(returnData)
+    }
+
+    this.getFamily = function() {
+      return $http
+        .get(host + '/families/current')
+        .then(returnData)
+    }
+
+  }
+])
